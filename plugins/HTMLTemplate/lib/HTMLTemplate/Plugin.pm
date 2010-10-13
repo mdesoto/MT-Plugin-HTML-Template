dbc4c0190eab92f616820847991ba029359eb9e3
Michael De Soto <web@quarg.com>
2010-10-13 11:22:20 -0400

    Version 0.03: Added missing right curly bracket in convert_tags method. Added config setting toggling plug-in on and off at blog level. No need to post process templates if not required.

--- plugins/HTMLTemplate/lib/HTMLTemplate/Plugin.pm
+++ plugins/HTMLTemplate/lib/HTMLTemplate/Plugin.pm
@@ -11,28 +11,37 @@ use MT::Util qw/ trim /;
 sub convert_tags {
     my ($cb, %args) = @_;
 
-    my $tmpl = $args{'template'};
-    if ($tmpl && !$tmpl->build_dynamic) {
+    my $blog = $args{'blog'};
+    my $plugin = MT->component('HTMLTemplate');
 
-        my $blog = $args{'blog'};
-        my $plugin = MT->component('HTMLTemplate');
+#require Data::Dumper;
+#${$args{'content'}} = Data::Dumper::Dumper($plugin->get_config_value('is_active', 'blog:' . $blog->id))
 
-        unless ($blog) {
-            if (MT->config->DebugMode > 0) {
-                MT->log({message => 'No blog context passed to HTMLTemplate::Plugin::convert_tags.'});
+    
+    if ($plugin->get_config_value('is_active', 'blog:' . $blog->id)) {
+
+        my $tmpl = $args{'template'};
+
+        if ($tmpl && !$tmpl->build_dynamic) {
+
+            unless ($blog) {
+                if (MT->config->DebugMode > 0) {
+                    MT->log({message => 'No blog context passed to HTMLTemplate::Plugin::convert_tags.'});
+                }
+                return;
             }
-            return;
-        }
 
-        my $tags = trim($plugin->get_config_value('html_template', 'blog:' . $blog->id));
-        return unless $tags && $tags ne '';
+            my $tags = trim($plugin->get_config_value('html_template', 'blog:' . $blog->id));
+            return unless $tags && $tags ne '';
 
-        my @tags = ($tags =~ m/(\S+)/g);
-        foreach my $tag (@tags) {
-            my $html = ${$args{'content'}};
-            $html =~ s!<(/?)$tag(if|loop|unless|else|var|include)\b!<$1TMPL_\U$2!ig;
-            ${$args{'content'}} = $html;
+            my @tags = ($tags =~ m/(\S+)/g);
+            foreach my $tag (@tags) {
+                my $html = ${$args{'content'}};
+                $html =~ s!<(/?)$tag(if|loop|unless|else|var|include)\b!<$1TMPL_\U$2!ig;
+                ${$args{'content'}} = $html;
+            }
         }
     }
+}
 
 1;
