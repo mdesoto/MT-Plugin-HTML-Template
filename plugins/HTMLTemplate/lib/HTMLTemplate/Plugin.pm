8fb21e57c218a97fc9c806732622c6d1f4005255
Michael De Soto <web@quarg.com>
2010-10-14 11:23:09 -0400

    Moved blog context check to useful place. Corrected README copy. Advanced version number in yaml.

--- plugins/HTMLTemplate/lib/HTMLTemplate/Plugin.pm
+++ plugins/HTMLTemplate/lib/HTMLTemplate/Plugin.pm
@@ -12,20 +12,18 @@ sub convert_tags {
     my ($cb, %args) = @_;
 
     my $blog = $args{'blog'};
-    my $plugin = MT->component('HTMLTemplate');
+    unless ($blog) {
+        if (MT->config->DebugMode > 0) {
+            MT->log({message => 'No blog context passed to HTMLTemplate::Plugin::convert_tags.'});
+        }
+        return;
+    }
 
+    my $plugin = MT->component('HTMLTemplate');
     if ($plugin->get_config_value('is_active', 'blog:' . $blog->id)) {
 
         my $tmpl = $args{'template'};
-
         if ($tmpl && !$tmpl->build_dynamic) {
-
-            unless ($blog) {
-                if (MT->config->DebugMode > 0) {
-                    MT->log({message => 'No blog context passed to HTMLTemplate::Plugin::convert_tags.'});
-                }
-                return;
-            }
 
             my $tags = trim($plugin->get_config_value('html_template', 'blog:' . $blog->id));
             return unless $tags && $tags ne '';
